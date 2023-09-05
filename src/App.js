9422f6c56131b6e38d47d355869c30b1628c07c7
Max <maximesin21@gmail.com>
2023-09-05 12:26:25 +0700

    Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import "./App.css";
+import styles from "./App.css";
 import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Main from "./pages/Main";
 
 function App() {
   return (
-    <div>
+    <div className={styles.appContainer}>
+      <Header />
       <Routes>
         <Route path="/" element={<Main />} />
       </Routes>
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./Header.module.scss";
+import logo from "../images/logo.jpeg";
+
+const Header = () => {
+  return (
+    <div className={styles.header__container}>
+      <img src={logo} alt="It easy logo" className={styles.header__img} />
+      <p className={styles.header__companyName}>It easy international</p>
+      <nav className={styles.header__nav}>
+        <ul className={styles.header__navList}>
+          <li className={styles.header__navItem}>ИТ-Услуги</li>
+          <li className={styles.header__navItem}>Наши продукты</li>
+          <li className={styles.header__navItem}>О компании</li>
+          <li className={styles.header__navItem}>Контакты</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
