ca8d05a22738363cb9971b4134d1aa9ab2bb2d3c
Max <maximesin21@gmail.com>
2023-09-06 12:25:51 +0700

    Contact Us pane

---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Button from "../UI/UX/button/Button";
+import styles from "./ContactUs.module.scss";
+
+const ContactUs = () => {
+  return (
+    <div className={styles.contactUs__container}>
+      <p className={styles.contactUs__text}>
+        Свяжитесь с нами чтобы узнать больше
+      </p>
+      <Button text="СВЯЗАТЬСЯ С НАМИ" />
+    </div>
+  );
+};
+
+export default ContactUs;
