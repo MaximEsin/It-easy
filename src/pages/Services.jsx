574dbea4cd80f03d0d5c6b8e729fb4138b06f130
Max <maximesin21@gmail.com>
2023-09-13 12:38:40 +0700

    Services in dev

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import styles from "../styles/Services.module.scss";
 
 const Services = () => {
-  return <div></div>;
+  return (
+    <div className={styles.services__container}>
+      <p>Page is in development</p>
+    </div>
+  );
 };
 
 export default Services;
